27d895826737dc77ba6a40389cbe33d3f19d366d
EriCatillon <ericatillon@gmail.com>
2015-04-13 14:12:37 +0200

    test

---
+++ src/AppBundle/Form/ImageType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ImageType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('filename', 'file')
+            ->add('title')
+            ->add('description')
+            ->add('filesize')
+            ->add('mimetype')
+            ->add('isPublished')
+            ->add('dateCrea')
+            ->add('dateMod')
+            ->add('Upload !', 'submit')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Image'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'appbundle_image';
+    }
+}
